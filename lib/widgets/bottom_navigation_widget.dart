d2efa8c2c418f2f7ff55a6fcc05d4d5de7ee12a7
asrim10 <asrimmsuwall@gmail.com>
2025-12-15 21:16:55 +0545

    setState in home of button navigation

--- lib/widgets/bottom_navigation_widget.dart
+++ lib/widgets/bottom_navigation_widget.dart
@@ -1,20 +1,35 @@
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 
-class BottomNavigationWidget extends StatelessWidget {
+class BottomNavigationWidget extends StatefulWidget {
   const BottomNavigationWidget({super.key});
 
   @override
+  State<BottomNavigationWidget> createState() => _BottomNavigationWidgetState();
+}
+
+class _BottomNavigationWidgetState extends State<BottomNavigationWidget> {
+  int _currentIndex = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: CurvedNavigationBar(
-        items: [
-          Icon(Icons.home, size: 30),
-          Icon(Icons.favorite, size: 30),
-          Icon(Icons.location_on, size: 30),
-          Icon(Icons.person, size: 30),
-        ],
-      ),
+    return CurvedNavigationBar(
+      index: _currentIndex,
+      backgroundColor: Colors.transparent,
+      color: Theme.of(context).primaryColor,
+      buttonBackgroundColor: Theme.of(context).primaryColor,
+      height: 56,
+      items: const [
+        Icon(Icons.home, size: 28, color: Colors.white),
+        Icon(Icons.favorite, size: 28, color: Colors.white),
+        Icon(Icons.location_on, size: 28, color: Colors.white),
+        Icon(Icons.person, size: 28, color: Colors.white),
+      ],
+      onTap: (index) {
+        setState(() {
+          _currentIndex = index;
+        });
+      },
     );
   }
 }
