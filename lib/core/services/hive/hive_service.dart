80ef86b8bbacfa3b11dee581358e82925e0d2672
asrim10 <asrimmsuwall@gmail.com>
2026-01-01 14:46:15 +0545

    hive_service_with_auth_CRUD

---
+++ lib/core/services/hive/hive_service.dart
@@ -0,0 +1,68 @@
+import 'package:hive/hive.dart';
+import 'package:hotelspot/core/constants/hive_table_constant.dart';
+import 'package:hotelspot/features/auth/data/models/auth_hive_model.dart';
+import 'package:path_provider/path_provider.dart';
+
+class HiveService {
+  Future<void> init() async {
+    final directory = await getApplicationCacheDirectory();
+    final path = "${directory.path}/${HiveTableConstant.dbName}";
+    Hive.init(path);
+    _registerAdapter();
+    await _openBoxes();
+  }
+
+  //Register all adapters
+  void _registerAdapter() {
+    if (!Hive.isAdapterRegistered(HiveTableConstant.authTypeId)) {
+      Hive.registerAdapter(AuthHiveModelAdapter());
+    }
+  }
+
+  //open all boxes
+  Future<void> _openBoxes() async {
+    await Hive.openBox<AuthHiveModel>(HiveTableConstant.authTable);
+  }
+
+  //clove all boxes
+  Future<void> close() async {
+    await Hive.close();
+  }
+
+  // =============== Auth CRUD Operations ====================
+
+  // Get auth box
+  Box<AuthHiveModel> get _authBox =>
+      Hive.box<AuthHiveModel>(HiveTableConstant.authTable);
+
+  //Register
+  Future<AuthHiveModel> registerUser(AuthHiveModel model) async {
+    await _authBox.put(model.authId, model);
+    return model;
+  }
+
+  //Login
+  Future<AuthHiveModel?> loginUser(String email, String password) async {
+    final users = _authBox.values.where(
+      (user) => user.email == email && user.password == password,
+    );
+    if (users.isNotEmpty) {
+      return users.first;
+    }
+    return null;
+  }
+
+  //logout
+  Future<void> logoutUser() async {}
+
+  //get current User
+  AuthHiveModel? getCurrentUser(String authId) {
+    return _authBox.get(authId);
+  }
+
+  //isEmail Exists
+  bool isEmailExists(String email) {
+    final users = _authBox.values.where((user) => user.email == email);
+    return users.isNotEmpty;
+  }
+}
