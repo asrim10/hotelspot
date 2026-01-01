a4b2857ad2bb0e2047ad6a44e000b9fed8eb45be
asrim10 <asrimmsuwall@gmail.com>
2026-01-01 14:24:08 +0545

    hive_table_constant

---
+++ lib/core/constants/hive_table_constant.dart
@@ -0,0 +1,13 @@
+class HiveTableConstant {
+  HiveTableConstant._();
+
+  //Database name
+
+  static const String dbName = "hotelspot_db";
+
+  static const int hotelId = 0;
+  static const String hotelTable = 'hotel_table';
+
+  static const int authTypeId = 1;
+  static const String authTable = 'auth_table';
+}
