dbcff82907c2363ea92217713856ed91e06d3e2b
asrim10 <asrimmsuwall@gmail.com>
2026-01-30 13:57:48 +0545

    hotel_view_model

---
+++ lib/features/hotel/presentation/view_model/hotel_viewmodel.dart
@@ -0,0 +1,67 @@
+import 'dart:io';
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hotelspot/features/hotel/domain/usecases/create_hotel_usecase.dart';
+import 'package:hotelspot/features/hotel/domain/usecases/upload_image_usecase.dart';
+import 'package:hotelspot/features/hotel/presentation/state/hotel_state.dart';
+
+class HotelViewmodel extends Notifier<HotelState> {
+  late final CreateHotelUsecase _createhotelUsecase;
+  late final UploadImageUsecase _uploadImageUsecase;
+
+  @override
+  HotelState build() {
+    _createhotelUsecase = ref.read(createHotelUsecaseProvider);
+    _uploadImageUsecase = ref.read(uploadImageProvider);
+    return const HotelState();
+  }
+
+  Future<void> createHotel({
+    required String hotelName,
+    required String location,
+    required double rating,
+    String? description,
+    File? image,
+    File? video,
+  }) async {
+    state = state.copyWith(status: HotelStatus.loading);
+
+    final result = await _createhotelUsecase(
+      CreateHotelParams(
+        name: hotelName,
+        location: location,
+        rating: rating,
+        description: description,
+        image: image,
+        video: video,
+      ),
+    );
+    result.fold(
+      (failure) => state = state.copyWith(
+        status: HotelStatus.error,
+        errorMessage: failure.message,
+      ),
+      (success) => {state = state.copyWith(status: HotelStatus.created)},
+    );
+  }
+
+  //upload image
+  Future<void> uploadImage(File image) async {
+    state = state.copyWith(status: HotelStatus.loading);
+    final result = await _uploadImageUsecase(UploadImageParams(image: image));
+    result.fold(
+      (failure) {
+        state = state.copyWith(
+          status: HotelStatus.error,
+          errorMessage: failure.message,
+        );
+      },
+      (imageName) {
+        state = state.copyWith(
+          status: HotelStatus.loaded,
+          uploadImageName: imageName,
+        );
+      },
+    );
+  }
+}
