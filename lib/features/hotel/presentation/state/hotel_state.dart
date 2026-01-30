2bffdfe7ef0ac6ce7edeceaaa8d4a13f7b01508a
asrim10 <asrimmsuwall@gmail.com>
2026-01-30 13:30:44 +0545

    hotel_state

---
+++ lib/features/hotel/presentation/state/hotel_state.dart
@@ -0,0 +1,46 @@
+import 'package:equatable/equatable.dart';
+import 'package:hotelspot/features/hotel/domain/entities/hotel_entity.dart';
+
+enum HotelStatus { initial, loading, loaded, error, created, updated, deleted }
+
+class HotelState extends Equatable {
+  final HotelStatus status;
+  final String? errorMessage;
+  final HotelEntity? selectedHotel;
+  final List<HotelEntity> hotels;
+  //store image name temp
+  final String? uploadImageName;
+
+  HotelState({
+    required this.status,
+    this.errorMessage,
+    this.selectedHotel,
+    required this.hotels,
+    this.uploadImageName,
+  });
+
+  HotelState copyWith({
+    HotelStatus? status,
+    String? errorMessage,
+    HotelEntity? selectedHotel,
+    List<HotelEntity>? hotels,
+    String? uploadImageName,
+  }) {
+    return HotelState(
+      status: status ?? this.status,
+      errorMessage: errorMessage ?? this.errorMessage,
+      selectedHotel: selectedHotel ?? this.selectedHotel,
+      hotels: hotels ?? this.hotels,
+      uploadImageName: uploadImageName ?? this.uploadImageName,
+    );
+  }
+
+  @override
+  List<Object?> get props => [
+    status,
+    errorMessage,
+    selectedHotel,
+    hotels,
+    uploadImageName,
+  ];
+}
